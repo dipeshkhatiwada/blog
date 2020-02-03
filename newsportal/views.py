967660d5d26b608689ca87b3f97d4d766351ca65
dipeshkhatiwada <deepaacee@gmail.com>
2020-02-03 10:53:03 +0545

    template setup

--- newsportal/views.py
+++ newsportal/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from blog.models import Blog
 
 
 def about(request):
     return render(request, 'about.html')
+
+
+def home(request):
+    data = Blog.objects.all()
+    context = {
+        'blogs': data
+    }
+    return render(request, 'home.html', context)
